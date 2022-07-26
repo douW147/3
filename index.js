43b4f07633ea575df74f533d53ebf0d0ccb62c69
Olexandr Stepanenko <sashok1111122222@gmail.com>
2022-07-26 19:33:16 +0300

    add: ability to play vs computer

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var counter = 0;
 var isGame = false;
 var isPlayAvailible = true;
 var isRefreshAvailible = false;
-var isVsComputer = true;
+var isVsComputer = false;
 const X = "X";
 const O = "O";
 const cellIdName = "cell";
@@ -13,7 +13,9 @@ const messageHeadingClassName = "message"
 const messageHeading = document.querySelector(`body > h1.${messageHeadingClassName}`);
 var cells = document.getElementsByClassName(cellClassName);
 const playButton = document.querySelector("body > div.controll-buttons > button:nth-child(1)");
-const refreshButton = document.querySelector("body > div.controll-buttons > button.controll-buttons__controll-button:nth-child(2)")
+const refreshButton = document.querySelector("body > div.controll-buttons > button:nth-child(2)")
+const vs1Button = document.querySelector("body > div.controll-buttons > button:nth-child(3)");
+const vsComputerButton = document.querySelector("body > div.controll-buttons > button:nth-child(4)");
 
 function onPlayClick () {
     if (!isGame && isPlayAvailible) {
@@ -25,19 +27,39 @@ function onPlayClick () {
     }
 }
 
-function resetProperties () {
-    field = ["", "", "", "", "", "", "", "", ""];
-    counter = 0; 
-    isGame = true;
-    messageHeading.innerHTML = "";
-    setCellsEmpty(cells);
-}
-
 function onRefreshClick () {
     if (isRefreshAvailible) {
         resetProperties (field, counter, isGame, messageHeading, cells);
     }
 }
+
+function on1Vs1Click() {
+    vs1Button.classList.remove(disableButtonClassName);
+    vsComputerButton.classList.add(disableButtonClassName);
+    isVsComputer = false;
+    resetProperties();
+}
+
+function on1vsComputerClick() {
+    vsComputerButton.classList.remove(disableButtonClassName);
+    vs1Button.classList.add(disableButtonClassName);
+    isVsComputer = true;
+    resetProperties();
+    console.log(isVsComputer);
+}
+
+function resetProperties () {
+    field = ["", "", "", "", "", "", "", "", ""];
+    counter = 0; 
+    isGame = true;
+    messageHeading.innerHTML = "";
+    isPlayAvailible = false;
+    isRefreshAvailible = true;
+    playButton.classList.add(disableButtonClassName);
+    refreshButton.classList.remove(disableButtonClassName);
+    setCellsEmpty(cells);
+}
+
 
 function setCellsEmpty (cells) {
     for (let index = 0; index < cells.length; index++) {
@@ -83,32 +105,61 @@ function isDraw (field) {
     return fieldsCounter === 9;
 }
 
-function onCellClick (event) {
-    if (isGame) {
-        const id = event.target.id.substring(4, 5) - 1;
-        const whichGoes = xOrO(counter);
-        if (field[id] === "" && (field[id] !== X && field[id] !== O)) {
-            field[id] = whichGoes;
-            counter+=1;
-            document.getElementById(`${cellIdName}${id+1}`).innerHTML = whichGoes;
+function isGameEnds () {
+    if (counter >= 4) {
+        if (isWin(X)) {
+            console.log("x wins");
+            messageHeading.innerHTML = "X wins";
+            isGame = false;
         }
-        if (counter >= 4) {
-            if (isWin(X)) {
-                console.log("x wins");
-                messageHeading.innerHTML = "X wins";
-                isGame = false;
-            }
-            if (isWin(O)) {
-                messageHeading.innerHTML = "O wins";
-                isGame = false;
-            }
-            if (isDraw(field)) {
-                messageHeading.innerHTML = "Draw";
-                isGame = false;
-            }
+        if (isWin(O)) {
+            messageHeading.innerHTML = "O wins";
+            isGame = false;
+        }
+        if (isDraw(field)) {
+            messageHeading.innerHTML = "Draw";
+            isGame = false;
         }
     }
 }
+
+function generateComputerId(){
+    while(true) {
+        const id = Math.floor(Math.random() * 8);
+        if (field[id] === "" && (field[id] !== X && field[id] !== O)) {
+            return id
+        }
+    }   
+}
+
+function computerGoes() {
+    console.log("lox")
+    if (isVsComputer && counter % 2 !== 0) {
+        const id = generateComputerId();
+        console.log(id);
+        makeStep(id);
+    }
+}
+
+function makeStep(id) {
+    const whichGoes = xOrO(counter);
+    if (field[id] === "" && (field[id] !== X && field[id] !== O)) {
+        field[id] = whichGoes;
+        counter+=1;
+        document.getElementById(`${cellIdName}${id+1}`).innerHTML = whichGoes;
+    }
+}
+
+function onCellClick (event) {
+    if (isGame) {
+        const id = event.target.id.substring(4, 5) - 1;
+        makeStep(id);
+        isGameEnds();
+        computerGoes(); // executes only if isVsComputer
+        isGameEnds();
+    }
+}
+
 
 
 
